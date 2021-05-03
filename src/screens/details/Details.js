160097be21adb3cd246bcc341b8873a15fb06251
hellosibun <sibun.soumya@gmail.com>
2021-05-03 21:59:12 +0530

    Added ‘Back to Home’ button and YouTube trailer on details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,8 +1,11 @@
 import { Typography } from '@material-ui/core';
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import Header from '../../common/header/Header';
 import moviesData from '../../common/movieData';
 import './Details.css';
+import Home from '../../screens/home/Home';
+import YouTube from 'react-youtube';
 
 class Details extends Component {
     constructor() {
@@ -21,11 +24,29 @@ class Details extends Component {
         
     }
 
+    backtohomeHandler = () => {
+        ReactDOM.render(<Home/>, document.getElementById('root'));
+    }
+
     render() {
         let movie = this.state.movie;
+        const opts = {
+            height: '300',
+            width: '700' ,
+            playerVars : {
+                autoplay: 1
+            }        
+        }
         return(
         <div className="details">
             <Header />
+            <div className="back">
+                <Typography onClick={this.backtohomeHandler}>
+                    &#60; Back to Home
+                </Typography>
+                
+            </div>
+
             <div className="flex-containerDetails">
                 <div className="leftDetails">
                     <img src={movie.poster_url} alt={movie.title} />
@@ -66,6 +87,14 @@ class Details extends Component {
                         <Typography>
                             <span className="bold">Plot: </span><a href={movie.wiki_url}></a>{movie.storyline}
                         </Typography>
+                    </div>
+
+                    <div className="trailerContainer">
+                        <Typography><span className="bold">Trailer: </span></Typography>
+                        <YouTube
+                            videoId={movie.trailer_url.split("?v=")[1]}
+                            opts={opts}
+                            onReady={this._onReady} />
                     </div>
 
                     
