81d04f20ed982afb5c6e11b812cdae575ff3520a
hellosibun <sibun.soumya@gmail.com>
2021-05-04 01:18:17 +0530

    Integrated released movies

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -4,7 +4,7 @@ import './Home.css';
 
 import Header from '../../common/header/Header';
 import { withStyles } from '@material-ui/core/styles';
-import moviesData from '../../common/movieData';
+
 import genres from '../../common/genres';
 import artists from '../../common/artists';
 import GridList from '@material-ui/core/GridList';
@@ -60,6 +60,7 @@ class Home extends Component {
         this.state= {
             movieName: "",
             upcomingMovies: [],
+            releasedMovies: [],
             genres : [],
             artists : []
         }
@@ -80,6 +81,20 @@ class Home extends Component {
         xhr.open("GET", this.props.baseUrl + "movies?status=PUBLISHED");
         xhr.setRequestHeader("Cache-Control", "no-cache");
         xhr.send(data);
+
+        let dataReleased = null;
+        let xhrReleased = new XMLHttpRequest();
+        xhrReleased.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                that.setState({
+                    releasedMovies: JSON.parse(this.responseText).movies
+                });
+            }
+        });
+
+        xhrReleased.open("GET", this.props.baseUrl + "movies?status=RELEASED");
+        xhrReleased.setRequestHeader("Cache-Control", "no-cache");
+        xhrReleased.send(dataReleased);
     }
 
     movieNameChangeHandler = event => {
@@ -118,7 +133,7 @@ class Home extends Component {
                 <div className="flex-container">
                     <div className="left">
                         <GridList cellHeight={350} cols={4} className={classes.gridListMain}>
-                            {moviesData.map(movie => (
+                        {this.state.releasedMovies.map(movie => (
                                 <GridListTile onClick={() => this.movieClickHandler(movie.id) } className="released-movie-grid-item" key={"grid" + movie.id}>
                                     <img src={movie.poster_url} className="movie-poster" alt={movie.title} />
                                     <GridListTileBar
